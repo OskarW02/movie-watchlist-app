63c6c2cf1c6a28b9f84c2d0f83761540cead7d93
oskar <oskarwiniarski.ow@gmail.com>
2026-05-10 18:01:49 +0200

    M1

---
+++ movie-review-frontend/src/components/MovieList.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  movies: Array
+})
+</script>
+
+<template>
+  <div>
+    <h2>Movie List</h2>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }} ({{ movie.year }})
+      </li>
+    </ul>
+  </div>
+</template>
--- src/main/java/de/htwberlin/MovieReview/webtech/controller/MovieController.java
+++ src/main/java/de/htwberlin/MovieReview/webtech/controller/MovieController.java
@@ -1,12 +1,14 @@
 package de.htwberlin.MovieReview.webtech.controller;
 
 import de.htwberlin.MovieReview.webtech.model.Movie;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class MovieController {
 
     @GetMapping("/movies")
