b7e82b980d585255c809170ada7198afa6db054d
oskar <oskarwiniarski.ow@gmail.com>
2026-05-11 16:06:07 +0200

    M1

---
+++ movie-review-frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import MovieList from './components/MovieList.vue'
+
+const movies = ref([])
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8080/movies')
+  movies.value = await response.json()
+})
+</script>
+
+<template>
+  <div>
+    <h1>Movie Review App</h1>
+
+    <MovieList :movies="movies" />
+  </div>
+</template>
