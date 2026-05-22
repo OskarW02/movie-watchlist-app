72081186a75ee09b92f86cbf6819c8fed7ac9296
oskar <oskarwiniarski.ow@gmail.com>
2026-05-22 18:45:13 +0200

    M1

--- src/main/java/de/htwberlin/MovieReview/webtech/controller/MovieController.java
+++ src/main/java/de/htwberlin/MovieReview/webtech/controller/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class MovieController {
 
     @GetMapping("/movies")
