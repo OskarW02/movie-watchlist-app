846cf0ca40d5aace57131bf2e41ae9e7826b5def
oskar <oskarwiniarski.ow@gmail.com>
2026-05-23 11:48:41 +0200

    M1

--- movie-review-frontend/src/App.vue
+++ movie-review-frontend/src/App.vue
@@ -5,7 +5,7 @@ import MovieList from './components/MovieList.vue'
 const movies = ref([])
 
 onMounted(async () => {
-  const response = await fetch('http://localhost:8080/movies')
+  const response = await fetch('https://movie-watchlist-backend-qpbn.onrender.com/movies')
   movies.value = await response.json()
 })
 </script>
