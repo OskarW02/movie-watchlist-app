2dd897c0ef5bc636321a0a7e8cac1bc34b00f49b
oskar <oskarwiniarski.ow@gmail.com>
2026-06-14 14:26:16 +0200

    M1

--- src/main/java/de/htwberlin/MovieReview/webtech/model/Movie.java
+++ src/main/java/de/htwberlin/MovieReview/webtech/model/Movie.java
@@ -1,10 +1,25 @@
 package de.htwberlin.MovieReview.webtech.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
     private int year;
+
+    public Movie() {
+    }
 
     public Movie(Long id, String title, int year) {
         this.id = id;
@@ -22,5 +37,17 @@ public class Movie {
 
     public int getYear() {
         return year;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
