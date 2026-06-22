384cdce099103002cebfa91cc7ec4f2a79240df3
oskar <oskarwiniarski.ow@gmail.com>
2026-06-22 11:59:26 +0200

    M1

--- movie-review-frontend/src/App.vue
+++ movie-review-frontend/src/App.vue
@@ -1,61 +1,3 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import axios from 'axios'
-import MovieList from './components/MovieList.vue'
-
-const API_URL = 'https://movie-watchlist-backend-vao3.onrender.com/movies'
-
-const movies = ref([])
-
-const newMovie = ref({
-  title: '',
-  rating: 1,
-  releaseYear: new Date().getFullYear()
-})
-
-// GET: Filme aus Backend/DB laden
-const loadMovies = async () => {
-  try {
-    const response = await axios.get(API_URL)
-    movies.value = response.data
-  } catch (error) {
-    console.error('Fehler beim Laden der Filme:', error)
-  }
-}
-
-// POST: Neuen Film speichern
-const addMovie = async () => {
-  try {
-    const response = await axios.post(API_URL, newMovie.value)
-
-    movies.value.push(response.data)
-
-    newMovie.value = {
-      title: '',
-      rating: 1,
-      releaseYear: new Date().getFullYear()
-    }
-  } catch (error) {
-    console.error('Fehler beim Speichern des Films:', error)
-  }
-}
-
-// DELETE: Film löschen
-const deleteMovie = async (id) => {
-  try {
-    await axios.delete(`${API_URL}/${id}`)
-
-    movies.value = movies.value.filter(movie => movie.id !== id)
-  } catch (error) {
-    console.error('Fehler beim Löschen des Films:', error)
-  }
-}
-
-onMounted(() => {
-  loadMovies()
-})
-</script>
-
 <template>
   <div>
     <h1>Movie Review App</h1>
@@ -73,11 +15,35 @@ onMounted(() => {
 
       <div>
         <label>Rating:</label>
-        <input v-model.number="newMovie.rating" type="number" min="1" max="5" required />
+
+        <button type="button" @click="showRatingPopup = true">
+          {{ newMovie.rating }} / 5 ⭐
+        </button>
       </div>
 
       <button type="submit">Film hinzufügen</button>
     </form>
+
+    <div v-if="showRatingPopup" class="popup-overlay">
+      <div class="popup">
+        <h3>Bewertung auswählen</h3>
+
+        <div class="rating-options">
+          <button
+            v-for="rating in 5"
+            :key="rating"
+            type="button"
+            @click="selectRating(rating)"
+          >
+            {{ rating }} ⭐
+          </button>
+        </div>
+
+        <button type="button" @click="showRatingPopup = false">
+          Abbrechen
+        </button>
+      </div>
+    </div>
 
     <MovieList
       :movies="movies"
